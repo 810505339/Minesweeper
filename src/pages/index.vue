d0ec59850fea2fcb331962bd15996ceb28804815
xueming <810505339@qq.com>
2022-05-21 21:38:30 +0800

    test

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,50 +1,23 @@
 <script setup lang="ts">
-const name = $ref('')
+interface BlockState {
+  x: number
+  y: number
 
-const router = useRouter()
-const go = () => {
-  if (name)
-    router.push(`/hi/${encodeURIComponent(name)}`)
 }
+
+const width = 10
+const height = 10
+
+const XYList = $ref<BlockState[][]>(Array.from(Array(width), (_, x) => Array.from(Array(height), (_, y) => ({ x, y }))))
+
 </script>
 
 <template>
   <div>
-    <div i-carbon-campsite text-4xl inline-block />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
-
-    <div py-4 />
-
-    <input
-      id="input"
-      v-model="name"
-      placeholder="What's your name?"
-      type="text"
-      autocomplete="false"
-      p="x-4 y-2"
-      w="250px"
-      text="center"
-      bg="transparent"
-      border="~ rounded gray-200 dark:gray-700"
-      outline="none active:none"
-      @keydown.enter="go"
-    >
-
-    <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
-      >
-        Go
-      </button>
+    <div v-for="(XList, x) in XYList" :key="x">
+      <div v-for="(block, y) in XList" :key="y">
+        {{ block }}
+      </div>
     </div>
   </div>
 </template>
